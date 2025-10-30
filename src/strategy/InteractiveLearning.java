c2c46e52164189a395c477e5deda7c1d80d02134
ulnazseitzhan <ulnazzzseitzh@gmail.com>
2025-10-30 12:00:09 +0500

    Implement LearningStrategy in InteractiveLearning

--- src/strategy/InteractiveLearning.java
+++ src/strategy/InteractiveLearning.java
@@ -1,4 +1,10 @@
 package strategy;
 
-public class InteractiveLearning {
+
+public class InteractiveLearning implements LearningStrategy {
+
+    @Override
+    public void learn(String courseTitle) {
+        System.out.println("Learning course \"" + courseTitle + "\" through interactive exercises and quizzes.");
+    }
 }
