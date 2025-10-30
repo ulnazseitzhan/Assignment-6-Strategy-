5892b5b81bc1c62486aaa231fa2ed1e23489907c
ulnazseitzhan <ulnazzzseitzh@gmail.com>
2025-10-30 12:01:33 +0500

    Implement LearningStrategy in VideoLearning class

--- src/strategy/VideoLearning.java
+++ src/strategy/VideoLearning.java
@@ -1,4 +1,9 @@
 package strategy;
 
-public class VideoLearning {
+public class VideoLearning implements LearningStrategy {
+
+    @Override
+    public void learn(String courseTitle) {
+        System.out.println("Learning course \"" + courseTitle + "\" by watching video lectures.");
+    }
 }
