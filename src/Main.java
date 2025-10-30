5694a31f7711a7a8ccddc8f3d0e4638205828331
ulnazseitzhan <ulnazzzseitzh@gmail.com>
2025-10-30 12:02:10 +0500

    Refactor Main class to implement learning strategies
    Updated the Main class to include course objects and learning strategies.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import model.Course;
+import strategy.VideoLearning;
+import strategy.InteractiveLearning;
+import strategy.LiveLearning;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //create course objects
+        Course javaCourse = new Course("Java Fundamentals");
+        Course aiCourse = new Course("Introduction to AI");
+
+        //assign different learning strategies
+        javaCourse.setLearningStrategy(new VideoLearning());
+        aiCourse.setLearningStrategy(new InteractiveLearning());
+
+        //start learning with initial strategies
+        javaCourse.startLearning();
+        aiCourse.startLearning();
+
+        System.out.println("\n--- Switching strategy at runtime ---\n");
+
+        //change strategy dynamically
+        javaCourse.setLearningStrategy(new LiveLearning());
+        javaCourse.startLearning();
     }
 }
