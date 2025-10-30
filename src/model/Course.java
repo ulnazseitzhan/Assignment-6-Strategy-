7be444fba1bffec8b38e2e9acd5ea7dce6c0d970
ulnazseitzhan <ulnazzzseitzh@gmail.com>
2025-10-30 11:59:32 +0500

    Implement learning strategy in Course class
    Added learning strategy functionality to Course class.

--- src/model/Course.java
+++ src/model/Course.java
@@ -1,4 +1,32 @@
 package model;
 
+import strategy.LearningStrategy;
+
 public class Course {
+
+    private String title;
+    private LearningStrategy learningStrategy;
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+
+    public void setLearningStrategy(LearningStrategy learningStrategy) {
+        this.learningStrategy = learningStrategy;
+    }
+
+
+    public void startLearning() {
+        if (learningStrategy == null) {
+            System.out.println("No learning strategy selected for the course \"" + title + "\".");
+            return;
+        }
+        learningStrategy.learn(title);
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
 }
